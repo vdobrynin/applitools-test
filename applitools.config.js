60f1657e41cf8ba632892b2eb32c38685e082537
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-26 21:34:46 -0700

    use different browser for applitools testing

--- applitools.config.js
+++ applitools.config.js
@@ -13,11 +13,11 @@ module.exports = {
     batchName: 'ngx demo visual testing',
 
     // // Select the browsers and devices to run your tests on via the Ultrafast Grid
-    // browser: [
-    //     { width: 800, height: 600, name: 'chrome' },
-    //     { width: 1600, height: 1200, name: 'firefox' },
-    //     { width: 1024, height: 768, name: 'safari' },
-    //     { deviceName: 'Pixel 2', screenOrientation: 'portrait' },
-    //     { deviceName: 'Nexus 10', screenOrientation: 'landscape' },
-    // ]
+    browser: [
+        { width: 800, height: 600, name: 'chrome' },
+        { width: 1600, height: 1200, name: 'firefox' },
+        { width: 1024, height: 768, name: 'safari' },
+        { deviceName: 'Pixel 2', screenOrientation: 'portrait' },
+        { deviceName: 'Nexus 10', screenOrientation: 'landscape' },
+    ]
 }
